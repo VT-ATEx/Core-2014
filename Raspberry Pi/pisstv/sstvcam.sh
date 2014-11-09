3038e470fc942235c77206547ab3277100f37f07
paulsk4 <paulsk4@vt.edu>
2014-11-09 13:22:47 -0500

    Raspberry Pi Files
    Code that is currently on the Pi for the SSTV stuff.  The sstvcam.sh
    file is the one that currently transmits the images taken, the other .sh
    in the pisstv folder is primarily for testing.  The pifm.c code is in
    the pisstv folder as well.  I changed the frequency and some clock
    properties in that.

---
+++ Raspberry Pi/pisstv/sstvcam.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# sstv camera, 
+# shell script to test SSTV camera
+
+### BEGIN INIT INFO
+# Provides:		sstvcam
+# Required-Start:	$remote_fs $syslog
+# Required-Stop:	$remote_fs $syslog
+# Default-Start:	2 3 4 5 6
+### END INIT INFO
+
+for i in 1 2 3
+do
+	raspistill -t 1 --width 320 --height 256 -e png -o /tmp/image.png
+	# add callsign
+	#/home/pi/ImageMagick-6.8.9-8/utilities/mogrify -pointsize 24 -draw "text 10,40 'KK4PWO'" /tmp/image.png
+	#/home/pi/pisstv/./pisstv /tmp/image.png 22050
+	/home/pi/pisstv/./pisstv /home/pi/pisstv/test.png 22050
+	#sudo /home/pi/pisstv/./pifm /tmp/image.png.wav 145.5 22050
+	sudo /home/pi/pisstv/./pifm /home/pi/pisstv/test.png.wav 145.5 22050
+	rm /tmp/image.png*
+	echo "$i pictures transmitted"
+done
