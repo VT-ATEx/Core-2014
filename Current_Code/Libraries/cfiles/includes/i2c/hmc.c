8f29cdbee5aa93297ab8295e16b94168b4be79cf
Peter Morrissey <pfm1995 [at] vt [dot] edu>
2015-02-06 15:38:43 -0500

    Changed magnetometer code over to new sensor.

---
+++ Current_Code/Libraries/cfiles/includes/i2c/hmc.c
@@ -0,0 +1,48 @@
+#include "mag3.h"
+
+//Peter Morrissey
+//2015.02.05
+//Contact pfm1995[at]vt[dot]edu with questions
+
+//Stars up I2C
+int* mag3Init(int addr, int bus) {
+	int*  i2cfd;
+	i2cfd = I2COpenBus(bus);
+	//If I2C fails to open, return error
+	if (*i2cfd < 0) return (int*)-1;
+	//If I2C fails to contact the sensor, return error
+	if (I2CStartSlave(i2cfd, addr) < 0) return (int*)-1;
+
+	return i2cfd;
+}
+
+//This is where I pull the data.
+Magnetics mag3GetData(int* i2cfd) {
+	//Create an array of six bytes to hold the data
+	char data[6];
+	//Create my struct
+	struct Magnetics foo = {0,0,0};
+
+	//If I2C returns the wrong number of bytes, quit with error
+	if(I2CRead(i2cfd, 6, data) != 6) {
+		perror("Read did not return the bytes specified");	
+	} else {
+		//Joins the two separate bytes into one intiger
+		//for each axis
+		foo.x = data[0];
+		foo.y = data[2];
+		foo.z = data[4];
+		foo.x << 8;
+		foo.y << 8;
+		foo.z << 8;
+		foo.x |= data[1];
+		foo.y |= data[3];
+		foo.z |= data[5];
+	}
+	return foo;
+}
+
+//Closes the I2C connection
+void mag3Close(int* i2cfd) {
+	I2CCloseBus(i2cfd);
+}
