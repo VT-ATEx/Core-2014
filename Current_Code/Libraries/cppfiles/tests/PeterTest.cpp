c3ace6b25d1af36390d016b4ed56c76b67a1e640
Peter Morrissey <pfm1995 [at] vt [dot] edu>
2015-02-20 11:13:30 -0500

    Added code for LIS331 and added comments to sensor control code

--- Current_Code/Libraries/cppfiles/tests/PeterTest.cpp
+++ Current_Code/Libraries/cppfiles/tests/PeterTest.cpp
@@ -13,6 +13,8 @@
 #include <pthread.h>
 #include <iostream>
 
+//Defines the various addresses and busses the sensors are on
+// COnfiguration-dependant, change as needed
 #define NUMTHRDS 6
 #define LM73_BUS 1
 #define LM73_ADDRESS 0x49
@@ -20,32 +22,63 @@
 #define HMC5993L_ADDRESS NULL
 #define CHIPCAP2_BUS 2
 #define CHIPCAP2_ADDRESS 0x28
+#define LIS331_BUS 2
+#define LIS331_ADDRESS 0x19
 
+//Thread control variables
 pthread_t callThd[NUMTHRDS];
 pthread_mutex_t mutexI2C1;
 pthread_mutex_t mutexI2C2;
 pthread_mutex_t mutexI2C3;
 
+//Functions to create threads and retrieve data
 void *LM73_retrieve(void *arg) {
+	//Creates output stream
 	ostream lm73_data;
+	//Creates sensor object
+	LM73 lm73(LM73_BUS, LM73_ADDRESS);
 
 	while(1) {
+		//Opens or creates file for data output
 		lm73_data.open ("lm73data.csv");
 
+		//Locks bus so only this thread can access it
 		pthread_mutex_lock (&mutexI2C1);
-		LM73 lm73(LM73_BUS, LM73_ADDRESS);
+		//Retrieves data
 		lm73_data << lm73.getTemperature();
+		//Closes bus
 		lm73.close();
+		//Unlocks bus
 		pthread_mutex_unlock (&mutexI2C1);
 
 		lm73_data << "\n";
+		//CLoses data file
 		lm73_data.close();
 	}
 	return 0;
 }
 
-void *GY521_retrieve(void *arg) {
-//TODO: Write GY521 code or see if old code will work
+void *LIS331_retrieve(void *arg) {
+	ostream lis331_data;
+	LIS331  lis331(LIS331_BUS, LIS331_ADDRESS);
+
+	pthread_mutex_lock (&mutexI2C2);
+	lis331.Configure(100, 12)
+	lis331.setpollrate(1000);
+	lis331.setdatapoints(100);
+	pthread_mutex_unlock (&mutexI2C2);
+
+	while(1) {
+		lis331_data.open ("lis331data.csv");
+		pthread_mutex_lock (&mutexI2C2);
+		lis331.start_update();
+		lis331_data << lis331.getXvalue() + "," +
+			lis331.getYvalue + "," + lis331.getZvalue +
+			"\n";
+		lis331.closer();
+		pthread_mutex_unlock();
+		lis331_data.close();
+	} 
 }
 
 void *ITG3200_retrieve(void *arg) {
@@ -121,26 +154,35 @@ void *BMP180_retrieve(void *arg) {
 
 //TODO: Write loop into sub-threads, not main
 int main (int argc, char *argv[]) {
+	//Creates a thread attribute
 	pthread_attr_t attr;
 	int i = 0;
 	void *status;
 
+	//Initializes thread blockers. These stop the threads from
+	//trying to access the same bus
 	pthread_mutex_init(&mutexI2C1, NULL);
 	pthread_mutex_init(&mutexI2C2, NULL);
 	pthread_mutex_init(&mutexI2C3, NULL);
 
+	//Initializes the attribute and defines it as 'joinable'
+	//This allows me to read the threads' end states
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHRED_CREATE_JOINABLE);
 
+	//Creates the threads
 	pthread_create(&callThd[0], &attr, LM73_retrieve, (void *)i);	
-	pthread_create(&callThd[1], &attr, GY521_retrieve, (void *)i);
+	pthread_create(&callThd[1], &attr, LIS331_retrieve, (void *)i);
 	pthread_create(&callThd[2], &attr, ITG3200_retrieve, (void *)i);
 	pthread_create(&callThd[3], &attr, HMC5883L_retrieve, (void *)i);
 	pthread_create(&callThd[4], &attr, ChipCap2_retrieve, (void *)i);
 	pthread_create(&callThd[5], &attr, BMP180_retrieve, (void *)i);
 
+	//Destroys the thread attribute
 	pthread_attr_destroy(&attr);
 	
+	//Calls the thread end condition if the thread has ended
+	//Otherwise, program pauses here until thread has ended
 	pthread_join(callThd[0], &status);
 	pthread_join(callThd[1], &status);
 	pthread_join(callThd[2], &status);
@@ -148,8 +190,10 @@ int main (int argc, char *argv[]) {
 	pthread_join(callThd[4], &status);
 	pthread_join(callThd[5], &status);
 
+	//Destroys the thread blockers
 	pthread_mutex_destroy(&mutexI2C1);
 	pthread_mutex_destroy(&mutexI2C2);
 	pthread_mutex_destroy(&mutexI2C3);
+	//Closes any remaining threads
 	pthread_exit(NULL);
 }
