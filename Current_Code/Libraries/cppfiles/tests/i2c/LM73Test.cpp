058d14a91f55e7d169b1f145df0fda15d90f0f39
unknown <philipthecow@yahoo.com>
2015-04-05 15:56:32 -0400

    Improved the LM73 temperature test code such that it compiles and runs.

--- Current_Code/Libraries/cppfiles/tests/i2c/LM73Test.cpp
+++ Current_Code/Libraries/cppfiles/tests/i2c/LM73Test.cpp
@@ -1,10 +1,14 @@
 #include "LM73.h"
+#include "LM73.cpp"
+#include "I2C.h"
+#include "I2C.cpp"
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
-	LM73 lm73(1, 0x49);
+	LM73 lm73(1, 0x48);
 
 	while(1) {
 
